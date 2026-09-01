f66229d195e8904d84267698427f30c8ae09c260
Sam Noel <smnoel10@gmail.com>
2026-09-01 15:36:34 -0400

    Fix loading state for heading icon, fix page title and description passing to BaseLayout

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Website Template";
+export const SITE_TITLE = "Noel Web Studio";
 export const SITE_DESCRIPTION = "This is a website template";
 
 export const BASE_IMAGE_PATH = "/src/assets/";
